437117a99f06e7a1fb5f1827ca703db037db5113
vinhvn001 <thevinhvn001@gmail.com>
2022-08-25 09:05:55 +0700

    update

--- src/main/java/elcom/ex1/LibraryBooks/repository/FirstLetterRepository.java
+++
@@ -1,7 +0,0 @@
-package elcom.ex1.LibraryBooks.repository;
-
-import elcom.ex1.LibraryBooks.entity.FirstLetter;
-import org.springframework.data.repository.CrudRepository;
-
-public interface FirstLetterRepository extends CrudRepository<FirstLetter, Long> {
-}
