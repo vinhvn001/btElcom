068dc48671a952fb89c2f1e66d98adf1421afdb5
vinhvn001 <thevinhvn001@gmail.com>
2022-08-29 16:33:19 +0700

    update

--- src/main/java/elcom/ex1/LibraryBooks/repository/BOOKSRepository.java
+++
@@ -1,8 +0,0 @@
-package elcom.ex1.LibraryBooks.repository;
-
-import elcom.ex1.LibraryBooks.entity.Books;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BooksRepository extends CrudRepository<Books, Long> {}
