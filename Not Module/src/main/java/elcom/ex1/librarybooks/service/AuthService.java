f76352dfafe672f91c2ffd388130c366027608f5
vinhvn001 <thevinhvn001@gmail.com>
2022-10-20 15:33:42 +0700

    update

--- Not Module/src/main/java/elcom/ex1/librarybooks/service/AuthService.java
+++
@@ -1,8 +0,0 @@
-package elcom.ex1.librarybooks.service;
-
-import org.springframework.security.core.userdetails.UserDetails;
-
-public interface AuthService {
-
-    UserDetails loadUserById(Long id);
-}
