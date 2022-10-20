ce202f5793949fea64e9e7b4187719d0234d0acd
vinhvn001 <thevinhvn001@gmail.com>
2022-10-20 16:25:49 +0700

    .

--- Not Module/src/main/java/elcom/ex1/librarybooks/repository/library/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package elcom.ex1.librarybooks.repository.library;
-
-import elcom.ex1.librarybooks.entity.library.Category;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends CrudRepository<Category, Long> {}
